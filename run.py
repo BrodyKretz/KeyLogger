e4f51fcc1b139abb87ad2374bcf65bf5f1250a48
BrodyKretz <43651169+BrodyKretz@users.noreply.github.com>
2021-03-04 14:46:45 -0700

    Update run.py

--- run.py
+++ run.py
@@ -30,18 +30,11 @@ def on_press(key):
     except AttributeError:
         logging.info('special key {0} pressed'.format(
             key))
-'''
-def on_release(key):
-    logging.info('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-'''
+
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
-        on_release=on_release) as listener:
+        ) as listener:
     listener.join()
 
 # ...or, in a non-blocking fashion:
