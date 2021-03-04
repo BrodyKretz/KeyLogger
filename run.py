78c8c4ce7b6833a9464bbd987b3bc8e64e0e1e37
BrodyKretz <43651169+BrodyKretz@users.noreply.github.com>
2021-03-04 14:50:51 -0700

    Update run.py

--- run.py
+++ run.py
@@ -31,23 +31,23 @@ def on_press(key):
         logging.info('special key {0} pressed'.format(
             key))
         
-        
+  '''      
 def on_release(key):
     logging.info('{0} released'.format(
         key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-
+'''
 # Collect events until released
 with keyboard.Listener(
-        on_press=on_press,on_release=on_release
+        on_press=on_press,
         ) as listener:
     listener.join()
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
-    on_press=on_press,on_release=on_release
+    on_press=on_press,
   )
 listener.start()
 
